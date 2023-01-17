f739343d56c86e5ba42ed515f3d8331b8bdba3ea
JJERANEK <jj_marie_jeranek@yahoo.com>
2023-01-16 19:26:20 -0600

    Fixed Some

--- Unsolved/static/js/logic.js
+++ Unsolved/static/js/logic.js
@@ -7,67 +7,63 @@ function createMap (bikeStations) {
 
 
   // Create the tile layer that will be the background of our map.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
+  // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //   }).addTo(myMap);
 
   // Create a baseMaps object to hold the lightmap layer.
-  // var baseMaps = {
-  //   "Street Map": street,
-  //   "Topographic Map": topo
-  // };
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  // let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  //   attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  // });
+
+  let baseMaps = {
+    "Street Map": street
+
+  };
   
 
   // Create an overlayMaps object to hold the bikeStations layer.
-  // var overlayMaps = {
-  //   "State Population": states,
-  //   "City Population": cities
-  // };
+  let overlayMaps = {
+    "Bike Stations": bikeStations
+  };
   
 
   // Create the map object with options.
-  myMap = L.map("map", {
+  myMap = L.map("map-id", {
     center: newYorkCoords,
-    zoom: mapZoomLevel
+    zoom: mapZoomLevel,
+    layers:[street, bikeStations] 
   });
-};
+
   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-  // L.control.layers(baseMaps, overlayMaps, {
-  //   collapsed: false
-  // }).addTo(myMap);
-  // }
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+};
 // Create the createMarkers function.
 function createMarkers (response) {
   // Pull the "stations" property from response.data.
-  let station = response[i].stations
+  let stations = response.data.stations;
   // Initialize an array to hold the bike markers.
-  bikeMarkers = []
+  bikeMarkers = [];
   // Loop through the stations array.
-  for (let i = 0; i < stations.name; i++) {
-    // For each station, create a marker, and bind a popup with the station's name.
-    // bikeMarkers.push(
-    //   L.circle(stations[i].coordinates, {
-    //     stroke: false,
-    //     fillOpacity: 0.75,
-    //     color: "white",
-    //     fillColor: "white",
-    //     radius: markerSize(locations[i].state.population)
-    //   })
-    // )};
-    if (response[i].lat) {
+  for (let i = 0; i < stations.length; i++) {
+
+    if (stations[i].lat) {
 
     
     // Add the marker to the bikeMarkers array.
-      markers.addLayer(L.marker([response[i].lat, response[i].lon])
-      .bindPopup(response[i].name));
+      bikeMarkers.push(L.marker([stations[i].lat, stations[i].lon])
+      .bindPopup(stations[i].name));
   }
   // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
   let bikes = L.layerGroup(bikeMarkers);
   createMap(bikes)
 }};
 // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
-d3.json(url).then(data => {
-  console.log(data)
-  createMarkers(data);
-});
+d3.json(url).then(data => createMarkers(data));
 
